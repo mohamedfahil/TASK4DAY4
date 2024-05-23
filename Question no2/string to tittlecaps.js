fef436768f2b10c95134ce9d55f9b5839dece84f
mohamedfahil <mfahil26@gmail.com>
2024-05-23 21:17:37 +0530

    Add files via upload

---
+++ Question no2/string to tittlecaps.js
@@ -0,0 +1,10 @@
+
+var str="lionel messi"
+var tittle=titleCase = (str) => {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1); 
+    } 
+    return str.join(' ');
+  }   
+  console.log("The Tittle Cap Is:\n",tittle(str))   
---
+++ Question no2/sum_of_no in array.js
@@ -0,0 +1,10 @@
+var faahil=[1,2,3,4]
+var sum = (faahil)=>{
+    var sum = 0;
+         for(var i = 0 ; i< faahil.length ; i++){
+            sum = sum + faahil[i];
+          }
+          return sum;
+          }
+          console.log()
+          console.log(sum(faahil))
